756af44933327c140d29d28cf274cdb8ee4b1737
Harshith Varma <harshith.varma17@gmail.com>
2024-10-20 01:06:36 +0530

    Lint changes

--- NESEmulator/Test.cpp
+++ NESEmulator/Test.cpp
@@ -12,7 +12,7 @@ class NES6502_Test : public olc::PixelGameEngine
 public:
 	NES6502_Test() { sAppName = "NES6502 Test"; }
 
-	Bus nes;
+	Bus NES;
 	std::map<uint16_t, std::string> mapAsm;
 
 	std::string hex(uint32_t n, uint8_t d)	{
@@ -28,7 +28,7 @@ public:
 			std::string sOffset = "$" + hex(nAddr, 4) + ":";
 			for (int col = 0; col < nColumns; col++)
 			{
-				sOffset += " " + hex(nes.read(nAddr, true), 2);
+				sOffset += " " + hex(NES.read(nAddr, true), 2);
 				nAddr += 1;
 			}
 			DrawString(nRamX, nRamY, sOffset);
@@ -39,23 +39,23 @@ public:
 	void DrawCpu(int x, int y)	{
 		std::string status = "STATUS: ";
 		DrawString(x, y, "STATUS:", olc::WHITE);
-		DrawString(x + 64, y, "N", nes.CPU.status & NES6502::N ? olc::GREEN : olc::RED);
-		DrawString(x + 80, y, "V", nes.CPU.status & NES6502::V ? olc::GREEN : olc::RED);
-		DrawString(x + 96, y, "-", nes.CPU.status & NES6502::U ? olc::GREEN : olc::RED);
-		DrawString(x + 112, y, "B", nes.CPU.status & NES6502::B ? olc::GREEN : olc::RED);
-		DrawString(x + 128, y, "D", nes.CPU.status & NES6502::D ? olc::GREEN : olc::RED);
-		DrawString(x + 144, y, "I", nes.CPU.status & NES6502::I ? olc::GREEN : olc::RED);
-		DrawString(x + 160, y, "Z", nes.CPU.status & NES6502::Z ? olc::GREEN : olc::RED);
-		DrawString(x + 178, y, "C", nes.CPU.status & NES6502::C ? olc::GREEN : olc::RED);
-		DrawString(x, y + 10, "PC: $" + hex(nes.CPU.prog_ctr, 4));
-		DrawString(x, y + 20, "A: $" + hex(nes.CPU.a, 2) + "  [" + std::to_string(nes.CPU.a) + "]");
-		DrawString(x, y + 30, "X: $" + hex(nes.CPU.x, 2) + "  [" + std::to_string(nes.CPU.x) + "]");
-		DrawString(x, y + 40, "Y: $" + hex(nes.CPU.y, 2) + "  [" + std::to_string(nes.CPU.y) + "]");
-		DrawString(x, y + 50, "Stack P: $" + hex(nes.CPU.stack_ptr, 4));
+		DrawString(x + 64, y, "N", NES.CPU.status & NES6502::N ? olc::GREEN : olc::RED);
+		DrawString(x + 80, y, "V", NES.CPU.status & NES6502::V ? olc::GREEN : olc::RED);
+		DrawString(x + 96, y, "-", NES.CPU.status & NES6502::U ? olc::GREEN : olc::RED);
+		DrawString(x + 112, y, "B", NES.CPU.status & NES6502::B ? olc::GREEN : olc::RED);
+		DrawString(x + 128, y, "D", NES.CPU.status & NES6502::D ? olc::GREEN : olc::RED);
+		DrawString(x + 144, y, "I", NES.CPU.status & NES6502::I ? olc::GREEN : olc::RED);
+		DrawString(x + 160, y, "Z", NES.CPU.status & NES6502::Z ? olc::GREEN : olc::RED);
+		DrawString(x + 178, y, "C", NES.CPU.status & NES6502::C ? olc::GREEN : olc::RED);
+		DrawString(x, y + 10, "PC: $" + hex(NES.CPU.prog_ctr, 4));
+		DrawString(x, y + 20, "A: $" + hex(NES.CPU.a, 2) + "  [" + std::to_string(NES.CPU.a) + "]");
+		DrawString(x, y + 30, "X: $" + hex(NES.CPU.x, 2) + "  [" + std::to_string(NES.CPU.x) + "]");
+		DrawString(x, y + 40, "Y: $" + hex(NES.CPU.y, 2) + "  [" + std::to_string(NES.CPU.y) + "]");
+		DrawString(x, y + 50, "Stack P: $" + hex(NES.CPU.stack_ptr, 4));
 	}
 
 	void DrawCode(int x, int y, int nLines)	{
-		auto it_a = mapAsm.find(nes.CPU.prog_ctr);
+		auto it_a = mapAsm.find(NES.CPU.prog_ctr);
 		int nLineY = (nLines >> 1) * 10 + y;
 		if (it_a != mapAsm.end()) {
 			DrawString(x, nLineY, (*it_a).second, olc::CYAN);
@@ -67,7 +67,7 @@ public:
 			}
 		}
 
-		it_a = mapAsm.find(nes.CPU.prog_ctr);
+		it_a = mapAsm.find(NES.CPU.prog_ctr);
 		nLineY = (nLines >> 1) * 10 + y;
 		if (it_a != mapAsm.end()) {
 			while (nLineY > y) {
@@ -107,18 +107,18 @@ public:
 		while (!ss.eof()) {
 			std::string b;
 			ss >> b;
-			nes.RAM[nOffset++] = (uint8_t)std::stoul(b, nullptr, 16);
+			NES.RAM[nOffset++] = (uint8_t)std::stoul(b, nullptr, 16);
 		}
 
 		// Set Reset Vector
-		nes.RAM[0xFFFC] = 0x00;
-		nes.RAM[0xFFFD] = 0x80;
+		NES.RAM[0xFFFC] = 0x00;
+		NES.RAM[0xFFFD] = 0x80;
 
 		// Extract dissassembly
-		mapAsm = nes.CPU.disassemble(0x0000, 0xFFFF);
+		mapAsm = NES.CPU.disassemble(0x0000, 0xFFFF);
 
 		// Reset
-		nes.CPU.reset();
+		NES.CPU.reset();
 		return true;
 	}
 
@@ -127,18 +127,18 @@ public:
 
 		if (GetKey(olc::Key::SPACE).bPressed) {
 			do {
-				nes.CPU.clock();
-			} while (!nes.CPU.cyclesLeft == 0);
+				NES.CPU.clock();
+			} while (!NES.CPU.cyclesLeft == 0);
 		}
 
 		if (GetKey(olc::Key::R).bPressed)
-			nes.CPU.reset();
+			NES.CPU.reset();
 
 		if (GetKey(olc::Key::I).bPressed)
-			nes.CPU.interruptRequest();
+			NES.CPU.interruptRequest();
 
 		if (GetKey(olc::Key::N).bPressed)
-			nes.CPU.nonMaskableInterrupt();
+			NES.CPU.nonMaskableInterrupt();
 
 		// Draw Ram Page 0x00		
 		DrawRam(2, 2, 0x0000, 16, 16);
